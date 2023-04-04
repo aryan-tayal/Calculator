eb65ba2c85b3cd39ab7ef831de77fe3d0402f535
Aryan Tayal <bookworms@ariansh.com>
2023-04-04 09:21:33 +0530

    refactor to reducer

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,97 +1,11 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import CalculatorBtn from "./CalculatorBtn";
 import CalculatorScreen from "./CalculatorScreen";
-
-const buttons = [
-  {
-    value: 1,
-    type: "number",
-  },
-  {
-    value: 2,
-    type: "number",
-  },
-  {
-    value: 3,
-    type: "number",
-  },
-  {
-    value: 4,
-    type: "number",
-  },
-  {
-    value: 5,
-    type: "number",
-  },
-  {
-    value: 6,
-    type: "number",
-  },
-  {
-    value: 7,
-    type: "number",
-  },
-  {
-    value: 8,
-    type: "number",
-  },
-  {
-    value: 9,
-    type: "number",
-  },
-  {
-    value: 0,
-    type: "number",
-  },
-  {
-    value: "reset",
-    type: "reset",
-  },
-  { value: "del", type: "delete" },
-  { value: "/", type: "operator" },
-  { value: "*", type: "operator" },
-  { value: "+", type: "operator" },
-  { value: "-", type: "operator" },
-  { value: "=", type: "equal" },
-];
+import reducer from "./reducer";
+import buttons from "./buttons";
 
 const Calculator = () => {
-  const [calcText, setCalcText] = useState("");
-  const handleNumberClick = (e) => {
-    setCalcText((prev) => prev + e.target.innerText);
-  };
-  const handleResetClick = () => {
-    setCalcText("");
-  };
-  const handleDelClick = () => {
-    setCalcText((prev) => prev.slice(0, -1));
-  };
-  const handleOperatorClick = (e) => {
-    const operators = buttons
-      .filter((b) => b.type === "operator")
-      .reduce((prev, current) => {
-        prev.push(current.value);
-        return prev;
-      }, []);
-    if (operators.includes(calcText.slice(-1)) || !calcText.length) {
-      console.log("NO");
-    } else {
-      setCalcText((prev) => prev + e.target.innerText);
-    }
-  };
-  const handleEqualsClick = () => {
-    const operators = buttons
-      .filter((b) => b.type === "operator")
-      .reduce((prev, current) => {
-        prev.push(current.value);
-        return prev;
-      }, []);
-    if (operators.includes(calcText.slice(-1))) {
-      console.log("NO!");
-    } else {
-      setCalcText(eval(calcText));
-    }
-  };
+  const [{ calcText }, dispatch] = useReducer(reducer, { calcText: "" });
   return (
     <div>
       <CalculatorScreen text={calcText} />
@@ -102,7 +16,9 @@ const Calculator = () => {
               value={button.value}
               type={button.type}
               key={button.value}
-              onBtnClick={handleNumberClick}
+              onBtnClick={() =>
+                dispatch({ type: "NUMBER", value: button.value })
+              }
             />
           );
         } else if (button.type === "reset") {
@@ -111,7 +27,7 @@ const Calculator = () => {
               value={button.value}
               type={button.type}
               key={button.value}
-              onBtnClick={handleResetClick}
+              onBtnClick={() => dispatch({ type: "RESET" })}
             />
           );
         } else if (button.type === "delete") {
@@ -120,7 +36,7 @@ const Calculator = () => {
               value={button.value}
               type={button.type}
               key={button.value}
-              onBtnClick={handleDelClick}
+              onBtnClick={() => dispatch({ type: "DEL" })}
             />
           );
         } else if (button.type === "operator") {
@@ -129,7 +45,9 @@ const Calculator = () => {
               value={button.value}
               type={button.type}
               key={button.value}
-              onBtnClick={handleOperatorClick}
+              onBtnClick={() =>
+                dispatch({ type: "OPERATOR", value: button.value })
+              }
             />
           );
         } else if (button.type === "equal") {
@@ -138,35 +56,11 @@ const Calculator = () => {
               value={button.value}
               type={button.type}
               key={button.value}
-              onBtnClick={handleEqualsClick}
+              onBtnClick={() => dispatch({ type: "EQUALS" })}
             />
           );
         }
       })}
-      {/* <CalculatorBtn
-        value={1}
-        type="number"
-        key={1}
-        onBtnClick={handleBtnClick}
-      />
-      <CalculatorBtn
-        value={2}
-        type="number"
-        key={2}
-        onBtnClick={handleBtnClick}
-      />
-      <CalculatorBtn
-        value={3}
-        type="number"
-        key={3}
-        onBtnClick={handleBtnClick}
-      />
-      <CalculatorBtn
-        value={4}
-        type="number"
-        key={4}
-        onBtnClick={handleBtnClick}
-      /> */}
     </div>
   );
 };
---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+import buttons from "./buttons";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "NUMBER":
+      console.log("hi");
+      return { calcText: state.calcText + action.value };
+    case "RESET":
+      return { calcText: "" };
+    case "DEL":
+      return { calcText: state.calcText.slice(0, -1) };
+    case "OPERATOR":
+      const operators = buttons
+        .filter((b) => b.type === "operator")
+        .reduce((prev, current) => {
+          prev.push(current.value);
+          return prev;
+        }, []);
+      if (
+        operators.includes(state.calcText.slice(-1)) ||
+        !state.calcText.length
+      ) {
+        console.log("NO");
+      } else {
+        return { calcText: state.calcText + action.value };
+      }
+    case "EQUALS": {
+      const operators = buttons
+        .filter((b) => b.type === "operator")
+        .reduce((prev, current) => {
+          prev.push(current.value);
+          return prev;
+        }, []);
+      if (operators.includes(state.calcText.slice(-1))) {
+        console.log("NO!");
+      } else {
+        return { calcText: eval(state.calcText) };
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+};
+export default reducer;
