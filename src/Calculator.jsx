c39e2b4d87f5de95cc211a8fdf9998714c6782e5
Aryan Tayal <bookworms@ariansh.com>
2023-04-04 05:41:13 +0530

    add basic calculator screen component

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CalculatorBtn from "./CalculatorBtn";
+import CalculatorScreen from "./CalculatorScreen";
 
 const Calculator = () => {
   return (
     <div>
+      <CalculatorScreen text="1*2" />
       <CalculatorBtn value={1} type="number" key={1} />
       <CalculatorBtn value={2} type="number" key={2} />
       <CalculatorBtn value={3} type="number" key={3} />
